7b45d173944f65c46a3a65c75a8fc976257976d6
Mario Juguilon <mariojjuguilon@gmail.com>
2022-07-09 06:38:09 -0500

    Revert "added makefile"
    This reverts commit 780f69c098b4e39e29a5874e3f23952445404d69.

--- src/components/Todo/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Component: React.FC = () => {
-  return <></>;
-};
-
-export default Component;
--- src/constants/size.ts
+++
@@ -1,6 +0,0 @@
-export const PaperSizes = {
-  A4: [210, 297], // in millimeters
-  LETTER: [216, 279],
-  LEGAL: [216, 356],
-  TABLOID: [432, 279],
-};
