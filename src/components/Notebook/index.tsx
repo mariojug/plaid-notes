d6cbc07acd56457a414df02070029770fb29b6ac
Mario Juguilon <mariojjuguilon@gmail.com>
2022-07-09 07:27:31 -0500

    added typescript styled dependencies

--- src/components/Notebook/index.tsx
+++ src/components/Notebook/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { useAppContext } from "../../context/AppContext";
 
+const NotebookDiv = styled.div``;
+
 const Notebook: React.FC = () => {
   return <></>;
 };
